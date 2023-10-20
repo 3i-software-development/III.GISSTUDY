8104268a2ba3eff9f7b2ed28de7040d85b9b5abd
tuannmhe <TuanNMHE172118@fpt.edu.vn>
2023-10-20 17:37:07 +0700

    commit new code map

---
+++ myMap/JS/index.js
@@ -0,0 +1,243 @@
+var map;
+var vectorSource;
+var draw;
+var isDrawing = false;
+var polygons = []; // Mảng chứa các polygon đã vẽ
+var defaultPolygon; // Polygon ban đầu
+
+var geojson = {};
+
+function readJson() {
+    fetch('./VN_Huyen.geojson')
+        .then(response => response.json())
+        .then(data => {
+            // Use the 'data' variable which now contains the JSON data
+            geojson = data;
+            console.log(data);
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function addPolygon() {
+    // Kiểm tra xem vectorSource đã được tạo hay chưa
+    if (!vectorSource) {
+        vectorSource = new ol.source.Vector();
+    }
+
+    var features = new ol.format.GeoJSON().readFeatures(geojson, {
+        featureProjection: 'EPSG:3857', // Chuyển đổi hệ tọa độ sang EPSG:3857
+    });
+
+    vectorSource.clear(); // Xóa tất cả các đối tượng trên lớp vector
+    vectorSource.addFeatures(features); // Thêm các đối tượng mới vào lớp vector
+
+    // Tùy chỉnh hiển thị bản đồ để hiển thị tất cả polygon.
+    var extent = vectorSource.getExtent();
+    map.getView().fit(extent, map.getSize());
+}
+
+function addPolygonByName(name) {
+    // Kiểm tra xem vectorSource đã được tạo hay chưa
+if (!vectorSource) {
+vectorSource = new ol.source.Vector();
+}
+
+var features = new ol.format.GeoJSON().readFeatures(geojson, {
+featureProjection: 'EPSG:3857', // Chuyển đổi hệ tọa độ sang EPSG:3857
+});
+
+// Lọc các đối tượng có 'VARNAME_2' trùng với giá trị của biến 'name'
+var filteredFeatures = features.filter(function (feature) {
+return feature.get('VARNAME_2') === name;
+});
+
+vectorSource.clear(); // Xóa tất cả các đối tượng trên lớp vector
+
+// Tạo phong cách chỉ với đường viền màu đen
+var style = new ol.style.Style({
+stroke: new ol.style.Stroke({
+    color: 'black', // Màu đen cho đường viền
+    width: 2 // Độ rộng của đường viền
+})
+});
+
+// Gán phong cách cho các đối tượng trong filteredFeatures
+filteredFeatures.forEach(function (feature) {
+feature.setStyle(style);
+});
+
+vectorSource.addFeatures(filteredFeatures); // Thêm các đối tượng mới vào lớp vector
+
+if (filteredFeatures.length > 0) {
+// Nếu có các đối tượng được tìm thấy, tùy chỉnh hiển thị bản đồ để hiển thị chúng.
+var extent = vectorSource.getExtent();
+map.getView().fit(extent, map.getSize());
+} else {
+// Xử lý trường hợp không tìm thấy đối tượng
+console.log('Không tìm thấy đối tượng có VARNAME_2 là ' + name);
+}
+}
+
+
+
+function createMap() {
+    // Create a source for the features
+    vectorSource = new ol.source.Vector();
+
+    // Create a vector layer to display the features
+    var vectorLayer = new ol.layer.Vector({
+        source: vectorSource,
+    });
+
+    // Create a map
+    const key = "get_your_own_OpIi9ZULNHzrESv6T2vL";
+    const source = new ol.source.TileJSON({
+        url: `https://api.maptiler.com/maps/streets-v2/tiles.json?key=${key}`,
+        tileSize: 512,
+        crossOrigin: "anonymous",
+    });
+    const attribution = new ol.control.Attribution({
+        collapsible: false,
+    });
+
+    map = new ol.Map({
+        target: "map",
+        layers: [
+            new ol.layer.Tile({
+                source: source,
+            }),
+            vectorLayer, // Add the vector layer to the map
+        ],
+        controls: ol.control.defaults.defaults({ attribution: false }).extend([attribution]),
+        view: new ol.View({
+            constrainResolution: true,
+            center: ol.proj.fromLonLat([107.1400, 15.4000]),
+            zoom: 7,
+        }),
+    });
+
+    // Tạo polygon ban đầu
+    var polygonCoordinates = [
+    [102.1400, 23.4000],
+    [109.4600, 23.4000],
+    [109.4600, 8.1800],
+    [102.1400, 8.1800],
+    ];
+
+    defaultPolygon = new ol.Feature({
+        geometry: new ol.geom.Polygon([polygonCoordinates]).transform('EPSG:4326', 'EPSG:3857'),
+    });
+
+    vectorSource.addFeature(defaultPolygon);
+}
+
+
+// Function to check if the point is inside the polygons and add an icon at the clicked location
+function checkPointAndAddIcon(evt) {
+    if (isDrawing) {
+        return;
+    }
+    var selectedPoint = evt.coordinate;
+
+    var pointInsideAnyPolygon = false; // Biến để kiểm tra điểm có thuộc vào bất kỳ polygon nào không
+
+    // Kiểm tra điểm với polygon mặc định
+    var defaultPolygonGeometry = defaultPolygon.getGeometry();
+    if (defaultPolygonGeometry.intersectsCoordinate(selectedPoint)) {
+        console.log("Point is inside the default polygon.");
+        pointInsideAnyPolygon = true;
+        var iconFeature = new ol.Feature({
+            geometry: new ol.geom.Point(selectedPoint),
+        });
+
+        var iconStyle = new ol.style.Style({
+            image: new ol.style.Icon({
+                anchor: [0.5, 0.5],
+                src: 'https://openlayers.org/en/latest/examples/data/dot.png',
+                color: 'blue',
+                scale: 0.7,
+            }),
+        });
+
+        iconFeature.setStyle(iconStyle);
+        vectorSource.addFeature(iconFeature);
+    }
+
+    // Kiểm tra điểm với tất cả các polygon trong mảng
+    for (var i = 0; i < polygons.length; i++) {
+        var polygonGeometry = polygons[i].getGeometry();
+        if (polygonGeometry.intersectsCoordinate(selectedPoint)) {
+            console.log("Point is inside polygon " + i + ".");
+            pointInsideAnyPolygon = true;
+            var iconFeature = new ol.Feature({
+                geometry: new ol.geom.Point(selectedPoint),
+            });
+
+            var iconStyle = new ol.style.Style({
+                image: new ol.style.Icon({
+                    anchor: [0.5, 0.5],
+                    src: 'https://openlayers.org/en/latest/examples/data/dot.png',
+                    color: 'green',
+                    scale: 0.7,
+                }),
+            });
+
+            iconFeature.setStyle(iconStyle);
+            vectorSource.addFeature(iconFeature);
+            break; // Nếu điểm thuộc một polygon, không cần kiểm tra tiếp
+        }
+    }
+
+    if (!pointInsideAnyPolygon) {
+        console.log("Point is outside all polygons.");
+
+        // Add an icon at the clicked location
+        var iconFeature = new ol.Feature({
+            geometry: new ol.geom.Point(selectedPoint),
+        });
+
+        var iconStyle = new ol.style.Style({
+            image: new ol.style.Icon({
+                anchor: [0.5, 0.5],
+                src: 'https://openlayers.org/en/latest/examples/data/dot.png',
+                color: 'red',
+                scale: 0.7,
+            }),
+        });
+
+        iconFeature.setStyle(iconStyle);
+        vectorSource.addFeature(iconFeature);
+    }
+
+    var coordinateInfo = document.getElementById('coordinate-info');
+    coordinateInfo.innerHTML = 'Coordinates: ' + selectedPoint;
+}
+
+// Function to start drawing
+function startDrawing() {
+    if (!isDrawing) {
+        draw = new ol.interaction.Draw({
+            source: vectorSource,
+            type: "Polygon",
+        });
+        map.addInteraction(draw);
+        isDrawing = true;
+
+        // Listen to the `drawend` event to capture the drawn polygon
+        draw.on("drawend", function (event) {
+            var drawnPolygon = event.feature;
+            polygons.push(drawnPolygon); // Thêm polygon vào mảng
+        });
+    }
+}
+
+// Function to stop drawing
+function stopDrawing() {
+    if (isDrawing) {
+        map.removeInteraction(draw);
+        isDrawing = false;
+    }
+}
